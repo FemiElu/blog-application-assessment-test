e47e471f7819a57df95150f302b5ea8e0532f908
FemiElujoba <elujobaoluwafemi@gmail.com>
2022-11-19 17:25:39 +0100

    first blogging app commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './pages/blog/BlogPage';
+import Home from './pages/home/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Routes>
+        <Route  exact path='/' element={Home} />
+        <Route path='/blog/:id' element={Blog} />
+        <Route path="/" element={ <Navigate to="/"/> } />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
